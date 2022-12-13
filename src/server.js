a655c2071289becc19d8ed4169f8e8272382ff4f
Phuong <lephuong10142002@gmail.com>
2022-12-13 09:52:00 +0700

    video 24

--- src/server.js
+++ src/server.js
@@ -1,31 +1,17 @@
 const express = require("express"); // common js
-const path = require("path");
 require("dotenv").config();
-// import express from "express"; //es module
+const configViewEngine = require("./config/viewEngine");
+const webRouter = require("./routes/web");
 
 const app = express(); // khai báo app của express
 const port = process.env.PORT || 8081; // port => hardcode
 const hostname = process.env.HOST_NAME;
 
 //config template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-//config static files
-app.use(express.static(path.join(__dirname, "public")));
+configViewEngine(app);
 
 // Khai báo route
-app.get("/", (req, res) => {
-  res.send("Hello World! & nodemon");
-});
-
-app.get("/abc", (req, res) => {
-  res.send("check ABC");
-});
-
-app.get("/test", (req, res) => {
-  res.render("sample.ejs");
-});
+app.use("/", webRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);

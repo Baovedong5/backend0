7826be96107b2a99e41fc218352245b57c5d9810
Phuong <lephuong10142002@gmail.com>
2022-12-13 14:53:19 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const configViewEngine = require("./config/viewEngine");
 const webRouter = require("./routes/web");
 
+const mysql = require("mysql2");
+
 const app = express(); // khai báo app của express
 const port = process.env.PORT || 8081; // port => hardcode
 const hostname = process.env.HOST_NAME;
@@ -12,6 +14,22 @@ configViewEngine(app);
 
 // Khai báo route
 app.use("/", webRouter);
+
+//test connection
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, //neu khong chuyen mac dinh la 3306
+  user: "root", // khong chuyen password default la empty
+  password: "123456",
+  database: "hoidanit",
+});
+
+//simple query
+connection.query("select * from Users", function (err, results, fields) {
+  console.log(">>> results= ", results); // results contains rows returned by server
+  console.log(">>>fields= ", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);

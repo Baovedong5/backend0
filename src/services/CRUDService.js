4210f28e7ef198d4f5961513112040b4a5aecd43
Phuong <lephuong10142002@gmail.com>
2022-12-14 15:45:49 +0700

    video 40

---
+++ src/services/CRUDService.js
@@ -0,0 +1,10 @@
+const connection = require("../config/dataBase");
+
+const getAllUsers = async () => {
+  let [results, fields] = await connection.query("select * from Users");
+  return results;
+};
+
+module.exports = {
+  getAllUsers,
+};
